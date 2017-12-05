fa546341e5352826cf5b71be67f9d7d3dde24fca
Yassine Belmamoun <yassine@pample.co>
2017-12-05 15:49:35 +0100

    Reset STATIC_ROOT settings variable

--- cas/settings.py
+++ cas/settings.py
@@ -15,6 +15,7 @@ import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Read the .env file
 dotenv_path = os.path.join(BASE_DIR, '.env')
@@ -123,7 +124,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
